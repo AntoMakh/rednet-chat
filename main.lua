c3096c2ced44bf3f97541606d623a5863c321387
Antonio <145848219+AntoMakh@users.noreply.github.com>
2025-06-01 23:44:57 +0300

    main code

---
+++ main.lua
@@ -0,0 +1,52 @@
+local modem = peripheral.find("modem")
+local modemName = peripheral.getName(modem)
+
+rednet.open(modemName)
+
+if rednet.isOpen(modem) then
+    print("RedNet is working!")
+end
+
+write("Hello. Are you Harvek (1) or Chris_A_75 (2)?")
+local user = tonumber(read())
+
+while user ~= 1 or user ~= 2 do
+    write("Hello. Are you Harvek (1) or Chris_A_75 (2)?")
+    local user = tonumber(read())
+end
+
+local receiverId
+
+if user == 1 then
+    receiverId = 8
+else
+    receiverId = 7
+end
+
+
+local function sendLoop()
+    while true do
+        local msg = read()
+        
+        if msg == "exit" then
+            rednet.close(modemName)
+            break
+        end
+        
+        local finalMsg = (receiverId == 7 and "<Harvek> " or "<Chris_A_75> ") .. msg
+        rednet.send(receiverId, finalMsg, "MethLab")
+        
+    end
+end
+
+local function receiveLoop()
+    while true do
+        local senderId, message, protocol = rednet.receive("MethLab")
+        if senderId == receiverId then
+            print("\n".. message)
+            write("> ")
+        end
+    end
+end
+
+parallel.waitForAny(sendLoop, receiveLoop)
